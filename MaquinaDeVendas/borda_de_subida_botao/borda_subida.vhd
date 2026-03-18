d8ed42e6f0b4927aca0f2ed0741241328276cc58
Vidal Daniel Cabrera Bernal <ra272631@snake.lab.ic.unicamp.br>
2026-03-18 16:37:52 -0300

    adiciona botao de borda de subida

---
+++ MaquinaDeVendas/borda_de_subida_botao/borda_subida.vhd
@@ -0,0 +1,40 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity borda_subida is
+    port (
+        clk   : in  std_logic;
+        rst   : in  std_logic;
+        entrada : in  std_logic;
+        saida : out std_logic
+    );
+end entity borda_subida;
+
+architecture rtl of borda_subida is
+    signal entrada_reg : std_logic;
+    signal respondido : std_logic;
+begin
+    process(clk, rst)
+    begin
+        if rst = '1' then
+            entrada_reg <= '0';
+            respondido <= '0';
+            saida <= '0';
+        elsif rising_edge(clk) then
+            entrada_reg <= entrada;
+            
+            -- Se entrada voltou a 0, reseta a flag
+            if entrada = '0' then
+                respondido <= '0';
+            end if;
+            
+            -- Detecta borda de subida e ainda não respondeu
+            if entrada = '1' and entrada_reg = '0' and respondido = '0' then
+                saida <= '1';
+                respondido <= '1';
+            else
+                saida <= '0';
+            end if;
+        end if;
+    end process;
+end architecture rtl;
