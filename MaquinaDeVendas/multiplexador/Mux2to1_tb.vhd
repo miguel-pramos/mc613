70c1e8822e3921f9c5679a252492d39dfe1eebc4
Fernando Nagano Foschiera <ra281183@kraft.lab.ic.unicamp.br>
2026-03-18 17:13:26 -0300

    feat: tb do mul2to1

--- MaquinaDeVendas/multiplexador/Mux2to1_tb.vhd
+++ MaquinaDeVendas/multiplexador/Mux2to1_tb.vhd
@@ -20,4 +20,28 @@ begin
         );
     stim: process
     begin
+        -- Valores fixos para as entradas 
+        t_valor <= std_logic_vector(to_signed(500, 11));
+        t_modulo <= std_logic_vector(to_signed(100, 11));
+        
+        -- Seleciona 'valor'
+        t_S <= '0';
+        wait for 10 ns;
+
+        -- Muda valor com S em 0
+        t_valor <= std_logic_vector(to_signed(150,11));
+        wait for 10 ns;
+
+        -- Seleciona 'modulo'
+        t_S <= '1';
+        wait for 10 ns;
+
+        -- Muda valor com S em 1
+        t_valor ,= std_logic_vector(to_signed(900, 11));
+        wait for 10 ns;
+
+        wait;
+    end process;
+end architecture
+
         
